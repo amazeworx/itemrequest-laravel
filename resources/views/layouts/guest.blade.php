9ec725653d48e9a97d020181d7669ae340d967b6
Fransnico Susanto <fransnico@amazeworx.com>
2023-01-07 14:05:13 +0700

    update guest.blade

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="theme-color" content="#641AE5" />
+  <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+  <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -16,6 +19,14 @@
   <div class="font-sans text-gray-900 antialiased">
     {{ $slot }}
   </div>
+  <script src="{{ asset('/sw.js') }}"></script>
+  <script>
+    if (!navigator.serviceWorker.controller) {
+      navigator.serviceWorker.register("/sw.js").then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+    }
+  </script>
 </body>
 
 </html>

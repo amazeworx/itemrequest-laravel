2aa764472ddebec2fe7f7494978de794f8aeac54
Fransnico Susanto <fransnico@amazeworx.com>
2023-01-08 03:14:06 +0700

    remove theme color

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="theme-color" content="#641AE5" />
   <link rel="manifest" href="{{ asset('/manifest.json') }}">
   <link rel="icon" sizes="512x512" href="{{ asset('logo-512x512.png') }}">
   <link rel="apple-touch-icon" href="{{ asset('logo-512x512.png') }}">
